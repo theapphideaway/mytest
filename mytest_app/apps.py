16f59e18c86f730302c7826199502bdfc8ff28a4
Ian Schoenrock <32747586+theapphideaway@users.noreply.github.com>
2023-02-17 14:00:03 -0700

    Added test view

---
+++ mytest_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MytestAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mytest_app'
---
+++ mytest_app/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.index, name='index'),
+]
+
